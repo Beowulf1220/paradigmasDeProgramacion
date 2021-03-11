b0c49d25dc9a4c8cd8199968bb02ac27a8c9bfc3
edwin <a329549@uach.mx>
2021-03-11 11:14:35 -0700

    Actividad 4 - Ciclo con alert en la 5ta iteración

---
+++ Actividad 4/numeros.js
@@ -0,0 +1,10 @@
+for(let i = 1; i <= 10; i++){
+    console.log(i); if(i==5)
+    alert('Hola mundo desde el número 5');
+}
+
+function suma(){
+    console.log(5+3);
+}
+
+const suma2 = (a,b) => console.log(a + b);
