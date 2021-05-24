703f52577178ec804fe87c269809e1b327306773
edwin <a329549@uach.mx>
2021-05-23 23:25:54 -0600

    Segundo parcial

--- segundo_parcial/src/firebase.js
+++ segundo_parcial/src/firebase.js
@@ -1,16 +1,14 @@
 import firebase from 'firebase/app'
-import 'firebase/firestore'
+import "firebase/firestore"
 
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyAYBwDH6VHrNhcYa5EKJB2zlaT8izTulmo",
-    authDomain: "vue-todo-2021-febec.firebaseapp.com",
-    databaseURL: "https://vue-todo-2021-febec-default-rtdb.firebaseio.com",
-    projectId: "vue-todo-2021-febec",
-    storageBucket: "vue-todo-2021-febec.appspot.com",
-    messagingSenderId: "212877263435",
-    appId: "1:212877263435:web:7f586bac06e37880a043ce"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-export const db = firebase.firestore();
+const firebaseConfig = {
+    apiKey: "AIzaSyCLWCFpp2NM5GvqzQLL1WyPyypw8Rqw2S4",
+    authDomain: "libros-3b3e8.firebaseapp.com",
+    projectId: "libros-3b3e8",
+    storageBucket: "libros-3b3e8.appspot.com",
+    messagingSenderId: "794374889434",
+    appId: "1:794374889434:web:413323f95fc45399b42ae3"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  export const db = firebase.firestore()
